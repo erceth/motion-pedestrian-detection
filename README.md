4e73881723e2651d3aee168ec28a056b9a8ac899
Eric <erceth@gmail.com>
2021-08-26 18:23:53 -0600

    README, npm init

--- README.md
+++ README.md
@@ -1,7 +1,132 @@
-# inspiring blog posts
+# Motion Pedestrian Detection
 
-https://www.pyimagesearch.com/2015/06/01/home-surveillance-and-motion-detection-with-the-raspberry-pi-python-and-opencv/
-https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
-https://www.pyimagesearch.com/2014/11/17/non-maximum-suppression-object-detection-python/
-https://www.pyimagesearch.com/2015/11/09/pedestrian-detection-opencv/
+Motion Pedestrian Detection uses openCV to detect motion from a series of images and searches for pedestrians in the sections of images where motion is detected. This process saves on CPU performance making it better equipped to handle on smaller devices like the Raspberry PI.
 
+![demo](./gifs/pedestrians.gif)
+
+Box Descriptions:
+
+* yellow boxes - detected pedestrian
+* green box - motion detected
+* blue box - buffer area around the motion area that is scanned for a pedestrian. Set with motionPaddingCutoutPercent configuration.
+
+Detection only occurs when movement is detect. If the pedestrian is not moving, detection does not occur. This is a reasonable trade off because any pedestrian worth tracking is moving.
+
+In other words: It can't see you if you don't move.
+
+![demo](./gifs/cant-see-if-we-dont-move.gif)
+
+
+## Requirements
+* OpenCV
+* Python 3
+## Recommended
+* FFMPEG
+## Files Description
+* `detect.py` - The main script used to detect pedestrians based off motion.
+* `driver.py` - Demonstrates how to use `detect.py`. It starts an ffmpeg stream and sends images to `detect.py`. Possible to create your own driver based on this file.
+* `driver.js` - Demonstrates how to use `detect.py` using Node. The node version of `driver.py`. It starts an ffmpeg process and sends images to a python process `detect-wrapper.py`, which then passes images to `detect.py`. Possible to create your own driver based on this file.
+* `config.json` - Configuration file. Details below.
+
+## Configuration Details
+* pythonPath - Path to python executable. Needed if running Node driver.
+* imshow - Displays the detection results in using openCV's imshow function. Useful for debugging. Turn off in production and command line only environments. 1 is on, 0 is off.
+* writeOutput - Writes the detection results to file. One source of output that can be consumed by greater application that wraps around this project. 1 is on, 0 is off.
+* writeTransparentOutput - A mostly transparent PNG with boxes containing the regions of interest. The PNG can be superimposed on the output stream. 1 is on, 0 is off.
+* ip - The IP address of the the incoming RTSP stream.
+* path - The path of the RTSP stream.
+* detectFPS - The frames per second the detection will occur on the incoming stream
+* width - The width of the resolution of the RTSP stream. If outputs looks garbled, this setting might be wrong.
+* height - The height of the resolution of the RTSP stream. If outputs looks garbled, this setting might be wrong.
+* minPixelSize - The minimum pixel size an object needs to be in order to detect motion. All objects smaller than this will be ignored. May need to adjust as height and width change.
+* motionDeltaThreshold - The sensitivity of motion detection. Lower number results in more motion detection.
+* motionPaddingCutoutPercent - Controls the size of the buffer area around the motion area that is scanned for a pedestrian. Is set as a percentage (e.g. .2 is 20%).
+* cutOutHeightLimit - Limit on how short the padding area can be (blue box).
+* checkNLargestObjects - Number of motion areas to perform motion detection on. Setting to 1 will only motion detection on the largest area. Setting to 2 will perform motion detect on the two largest areas, etc.
+* windowStride - How quickly searching window parses an image. Lower number means high CPU intensive and more accurate. Higher number means less CPU intensive but less accurate. Min value of 1.
+* hogPadding - The number of pixels in both the x and y direction in which the sliding window ROI is “padded” prior to HOG feature extraction. Typical values for padding include (8, 8), (16, 16), (24, 24), and (32, 32)
+* hogScale - Controls the resize factor while process is repeatedly resizing image to find pedestrians. Smaller results in more resizes, more accurate and more CPU usage. Lower is less resizes, less accurate and less CPU usage. Minimum is 1.01.
+* hogHitThreshold - hog variable in determining overlapping detection. Unless you know what you are doing, set to 0 and use overlapThresh
+* overlapThresh - Used for non-maximum suppression, corrects for multiple detection of the same pedestrian. Amount of overlap per detection before counting it as a single detection.
+
+## Examples
+### Pedestrians:
+![demo](./gifs/pedestrians.gif)
+
+Config settings:
+```
+"detectParameters": {
+  "detectFps": 5,
+  "width": 854,
+  "height": 480,
+  "minPixelSize": 5000,
+  "motionDeltaThreshold": 2,
+  "motionPaddingCutoutPercent": 0.2,
+  "cutOutHeightLimit": 150,
+  "checkNLargestObjects": 2,
+  "windowStride": 4,
+  "hogPadding": 4,
+  "hogScale": 1.05,
+  "hogHitThreshold": 0,
+  "nonMaxSuppressionThreshold": 0.65
+}
+```
+
+### Walk:
+![demo](./gifs/walk.gif)
+
+Config settings:
+```
+"detectParameters": {
+  "detectFps": 5,
+  "width": 854,
+  "height": 480,
+  "minPixelSize": 5000,
+  "motionDeltaThreshold": 6,
+  "motionPaddingCutoutPercent": 0.2,
+  "cutOutHeightLimit": 150,
+  "checkNLargestObjects": 1,
+  "windowStride": 4,
+  "hogPadding": 4,
+  "hogScale": 1.05,
+  "hogHitThreshold": 0,
+  "nonMaxSuppressionThreshold": 0.65
+}
+```
+
+
+### Running on Raspberry Pi 3:
+
+Pi is able to handle both FFMPEG streaming and OpenCV motion detection and pedestrian detection.
+
+![demo](./gifs/eric-htop.gif)
+
+
+
+Config settings:
+```
+"detectParameters": {
+  "detectFps": 5,
+  "width": 854,
+  "height": 480,
+  "minPixelSize": 5000,
+  "motionDeltaThreshold": 6,
+  "motionPaddingCutoutPercent": 0.2,
+  "cutOutHeightLimit": 150,
+  "checkNLargestObjects": 1,
+  "windowStride": 4,
+  "hogPadding": 4,
+  "hogScale": 1.05,
+  "hogHitThreshold": 0,
+  "nonMaxSuppressionThreshold": 0.65
+}
+```
+
+
+
+# blog posts inspiration
+
+* https://www.pyimagesearch.com/2015/06/01/home-surveillance-and-motion-detection-with-the-raspberry-pi-python-and-opencv/
+* https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
+* https://www.pyimagesearch.com/2014/11/17/non-maximum-suppression-object-detection-python/
+* https://www.pyimagesearch.com/2015/11/09/pedestrian-detection-opencv/
--- detect.py
+++ detect.py
@@ -28,74 +28,77 @@ class Detect:
     self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
   def processFrame(self, frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    try:
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    self.transparent = np.zeros((self.height, self.width, 4), dtype=np.uint8)
-    if self.avg is None:
-      print("[INFO] starting background model...")
-      self.avg = gray.copy().astype("float")
-    # accumulate the weighted average between the current frame and
-    # previous frames, then compute the difference between the current
-    # frame and running average
-    cv2.accumulateWeighted(gray, self.avg, 0.5)
-    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(self.avg))
+      self.transparent = np.zeros((self.height, self.width, 4), dtype=np.uint8)
+      if self.avg is None:
+        print("[INFO] starting background model...")
+        self.avg = gray.copy().astype("float")
+      # accumulate the weighted average between the current frame and
+      # previous frames, then compute the difference between the current
+      # frame and running average
+      cv2.accumulateWeighted(gray, self.avg, 0.5)
+      frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(self.avg))
 
-    thresh = cv2.threshold(frameDelta, self.motionDeltaThreshold, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+      thresh = cv2.threshold(frameDelta, self.motionDeltaThreshold, 255, cv2.THRESH_BINARY)[1]
+      thresh = cv2.dilate(thresh, None, iterations=2)
+      cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+      cnts = imutils.grab_contours(cnts)
 
-    cntsSorted = sorted(cnts, key=cv2.contourArea, reverse=True)
+      cntsSorted = sorted(cnts, key=cv2.contourArea, reverse=True)
 
-    for c in cntsSorted[:self.checkNLargestObjects]:
-      # if the contour is too small, ignore it
-      if cv2.contourArea(c) < self.minPixelSize:
-        break
-      (x, y, w, h) = cv2.boundingRect(c)
-      paddingWidth = w * self.motionPaddingCutoutPercent
-      paddingHeight = h * self.motionPaddingCutoutPercent
-      padX1 = int(max(0, x - paddingWidth))
-      padY1 = int(max(0, y - paddingHeight))
-      padX2 = int(min(self.width, x + w + paddingWidth))
-      padY2 = int(min(self.height, y + h + paddingHeight))
+      for c in cntsSorted[:self.checkNLargestObjects]:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < self.minPixelSize:
+          break
+        (x, y, w, h) = cv2.boundingRect(c)
+        paddingWidth = w * self.motionPaddingCutoutPercent
+        paddingHeight = h * self.motionPaddingCutoutPercent
+        padX1 = int(max(0, x - paddingWidth))
+        padY1 = int(max(0, y - paddingHeight))
+        padX2 = int(min(self.width, x + w + paddingWidth))
+        padY2 = int(min(self.height, y + h + paddingHeight))
 
-      cutOut = frame[padY1:padY2, padX1:padX2]
-      if self.imshow == 1 or self.writeOutput == 1:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 2) # draw motion detection in green
-        cv2.rectangle(frame, (padX1, padY1), (padX2, padY2), (255,0,0), 2) # draw padded cutout in blue
+        cutOut = frame[padY1:padY2, padX1:padX2]
+        if self.imshow == 1 or self.writeOutput == 1:
+          cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 2) # draw motion detection in green
+          cv2.rectangle(frame, (padX1, padY1), (padX2, padY2), (255,0,0), 2) # draw padded cutout in blue
 
-      if w > 100 and h > 100:
-        ratio = 1
-        resizedCutOut = cutOut
-        if resizedCutOut.shape[0] > self.cutOutHeightLimit:
+        if w > 100 and h > 100:
+          ratio = 1
+          resizedCutOut = cutOut
+          if resizedCutOut.shape[0] > self.cutOutHeightLimit:
 
-          ratio = self.cutOutHeightLimit / resizedCutOut.shape[0]
-          dim = (int(resizedCutOut.shape[1] * ratio), self.cutOutHeightLimit)
-          resizedCutOut = cv2.resize(cutOut, dim, interpolation=cv2.INTER_AREA)
+            ratio = self.cutOutHeightLimit / resizedCutOut.shape[0]
+            dim = (int(resizedCutOut.shape[1] * ratio), self.cutOutHeightLimit)
+            resizedCutOut = cv2.resize(cutOut, dim, interpolation=cv2.INTER_AREA)
 
-        (rects, weights) = self.hog.detectMultiScale(resizedCutOut, winStride=(self.windowStride,self.windowStride), padding=(self.hogPadding, self.hogPadding), scale=self.hogScale, hitThreshold=self.hogHitThreshold)
+          (rects, weights) = self.hog.detectMultiScale(resizedCutOut, winStride=(self.windowStride,self.windowStride), padding=(self.hogPadding, self.hogPadding), scale=self.hogScale, hitThreshold=self.hogHitThreshold)
 
-        # unshrink, back to cutOut
-        rects = np.array([[int(x/ratio), int(y/ratio), int(x/ratio + w/ratio), int(y/ratio + h/ratio)] for (x, y, w, h) in rects])
-        pick = non_max_suppression(rects, probs=None, overlapThresh=self.nonMaxSuppressionThreshold)
-        if len(pick):
-          for (xA, yA, xB, yB) in pick:
-            # translate cutout measurements to main frame
-            cv2.rectangle(self.transparent, (padX1 + xA, padY1 + yA), (padX1 + xB, padY1 + yB), (0, 255, 255, 255), 2)
-            cv2.rectangle(frame, (padX1 + xA, padY1 + yA), (padX1 + xB, padY1 + yB), (0, 255, 255, 255), 2)
+          # unshrink, back to cutOut
+          rects = np.array([[int(x/ratio), int(y/ratio), int(x/ratio + w/ratio), int(y/ratio + h/ratio)] for (x, y, w, h) in rects])
+          pick = non_max_suppression(rects, probs=None, overlapThresh=self.nonMaxSuppressionThreshold)
+          if len(pick):
+            for (xA, yA, xB, yB) in pick:
+              # translate cutout measurements to main frame
+              cv2.rectangle(self.transparent, (padX1 + xA, padY1 + yA), (padX1 + xB, padY1 + yB), (0, 255, 255, 255), 2)
+              cv2.rectangle(frame, (padX1 + xA, padY1 + yA), (padX1 + xB, padY1 + yB), (0, 255, 255, 255), 2)
 
-        print(len(pick), flush=True)
+          print(len(pick), flush=True)
 
-    if self.imshow == 1:
-      self.show(frame, self.transparent)
+      if self.imshow == 1:
+        self.show(frame, self.transparent)
 
-    if self.writeOutput == 1:
-      self.write(frame, f'output{self.index}')
+      if self.writeOutput == 1:
+        self.write(frame, f'output{self.index}')
 
-    if self.writeTransparentOutput == 1:
-      self.write(self.transparent, f'output-detect{self.index}')
-
+      if self.writeTransparentOutput == 1:
+        self.write(self.transparent, f'output-detect{self.index}')
+    
+    except cv2.error as e:
+      print('python exception', e, flush=True)
     
 
   def show(self, frame, transparent):
